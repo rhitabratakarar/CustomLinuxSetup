571baf97856ad9eabcbd3d81256848c0852b0cf0
Rintu <rintu.karar@gmail.com>
2021-02-19 14:34:38 +0530

    Add files via upload

---
+++ colors.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ 
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://sam.zoy.org/wtfpl/COPYING for more details.
+printf "\nFor using one of the 256 colors on the foreground '(text color)', the control sequence is '<Esc>[38;5;ColorNumberm'\n\n"
+printf "For using one of the 256 colors on the background, the control sequence is '<Esc>[48;5;ColorNumberm'\n\n"
+for fgbg in 38 48 ; do # Foreground / Background
+    for color in {0..255} ; do # Colors
+        # Display the color
+        printf "\e[${fgbg};5;%sm  %3s  \e[0m" $color $color
+        # Display 6 colors per lines
+        if [ $((($color + 1) % 6)) == 4 ] ; then
+            echo # New line
+        fi
+    done
+    echo # New line
+done
+ 
